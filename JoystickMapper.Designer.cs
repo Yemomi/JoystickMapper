85fcead063d3ee314e3221c8642faf0e1fc33594
Yemomi <yewenjian@me.com>
2015-06-08 15:50:37 +0800

    Joystick Mapper

---
+++ JoystickMapper.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace JoystickMapper
+{
+    partial class JoystickMapper
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtbReport = new System.Windows.Forms.RichTextBox();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.lbDevices = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // rtbReport
+            // 
+            this.rtbReport.Location = new System.Drawing.Point(212, 12);
+            this.rtbReport.Name = "rtbReport";
+            this.rtbReport.Size = new System.Drawing.Size(235, 224);
+            this.rtbReport.TabIndex = 0;
+            this.rtbReport.Text = "";
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(12, 212);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(193, 23);
+            this.btnRead.TabIndex = 2;
+            this.btnRead.Text = "&Read";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // lbDevices
+            // 
+            this.lbDevices.FormattingEnabled = true;
+            this.lbDevices.ItemHeight = 12;
+            this.lbDevices.Location = new System.Drawing.Point(12, 12);
+            this.lbDevices.Name = "lbDevices";
+            this.lbDevices.Size = new System.Drawing.Size(193, 196);
+            this.lbDevices.TabIndex = 3;
+            // 
+            // JoystickMapper
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(459, 248);
+            this.Controls.Add(this.lbDevices);
+            this.Controls.Add(this.btnRead);
+            this.Controls.Add(this.rtbReport);
+            this.Name = "JoystickMapper";
+            this.Text = "JoystickMapper";
+            this.Load += new System.EventHandler(this.FrmJoystickMapper_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtbReport;
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.ListBox lbDevices;
+    }
+}
+
---
+++ JoystickMapper.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HidLibrary;
+
+namespace JoystickMapper
+{
+    public partial class JoystickMapper : Form
+    {
+        private HidDevice[] m_deviceList;
+        private HidDevice[] m_selectedDeviceList;
+        public delegate void ReadHandlerDelegate(HidReport report);
+
+
+        public JoystickMapper()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmJoystickMapper_Load(object sender, EventArgs e)
+        {
+            RefreshDevices();
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            if (lbDevices.SelectedIndex <= -1) return;
+
+            var device = m_deviceList[lbDevices.SelectedIndex];
+
+            device.OpenDevice();
+            device.MonitorDeviceEvents = true;
+
+            device.ReadReport(ReadProcess);
+        }
+
+        private void RefreshDevices()
+        {
+            m_deviceList = HidDevices.Enumerate().ToArray();
+            //_deviceList = HidDevices.Enumerate(0x536, 0x207, 0x1c7).ToArray();
+            lbDevices.DisplayMember = "Description";
+            lbDevices.DataSource = m_deviceList;
+            if (m_deviceList.Length > 0) m_selectedDeviceList[0] = m_deviceList[0];
+        }
+
+        private void ReadProcess(HidReport report)
+        {
+            BeginInvoke(new ReadHandlerDelegate(ReadHandler), new object[] { report });
+        }
+
+        private void ReadHandler(HidReport report)
+        {
+            //rtbReport.Clear();
+            rtbReport.AppendText(String.Format("{0}{1}",  String.Join(" ", report.Data.Select(d => d.ToString("X2"))), Environment.NewLine));
+
+            m_deviceList[lbDevices.SelectedIndex].ReadReport(ReadProcess);
+        }
+    }
+}
